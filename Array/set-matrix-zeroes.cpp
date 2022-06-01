824aa0ecfccf753a84cd3d1fe771c9efd0ff0647
rhythamm <106010620+rhythamm@users.noreply.github.com>
2022-06-01 14:09:08 +0530

    Update set-matrix-zeroes.cpp

--- Array/set-matrix-zeroes.cpp
+++ Array/set-matrix-zeroes.cpp
@@ -46,7 +46,7 @@ public:
 class Solution {
 public:
     //making 2 vectors for row and coloumn 
-    //if 0 is present in the matrix , we will mark 0 in row and col matrix at the same index as that of matrix
+    //if 0 is present in the matrix , we will mark 0 in row and col vector at the same index as that of matrix
     void setZeroes(vector<vector<int>>& matrix) {
         int i,j,k;
         vector<int> row(matrix.size(),-1);
@@ -81,4 +81,45 @@ public:
 
 //OPTIMIZED APPROACH 
 //taking 1st row from matrix as row dummy matrix and taking 1st column from the matrix as column dummy matrix
-
+class Solution {
+public:
+    //making 2 vectors for row and coloumn 
+    //if 0 is present in the matrix , we will mark 0 in row and col matrix at the same index as that of matrix
+    void setZeroes(vector<vector<int>>& matrix) {
+        int i,j,k;
+        int col0=1;
+        for(i=0;i<matrix.size();i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                col0=0;
+            }
+            for(j=1;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+    //traversing in the reverse direction and
+   //checking if the row or col has 0 or not
+   //and setting values of matrix accordingly.
+        for(i=matrix.size()-1;i>=0;i--)
+        {
+            for(j=matrix[0].size()-1;j>=1;j--)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+            if(col0==0)
+            {
+                matrix[i][0]=0;
+            }
+        }
+       
+    }
+};
